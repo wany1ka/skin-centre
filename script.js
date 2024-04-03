e2edd6f6337340de3b146a229b59eb992fd2a0ec
wany1ka <markwanyika@gmail.com>
2024-04-03 11:26:25 +0300

    edit blog

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleReadMore(moreId, cutId, buttonId) {
-  var moreText = document.getElementById(moreId);
-  var cutText = document.getElementById(cutId);
-  var btnText = document.getElementById(buttonId);
+  let moreText = document.getElementById(moreId);
+  let cutText = document.getElementById(cutId);
+  let btnText = document.getElementById(buttonId);
 
   if (moreText.style.display === "none") {
     moreText.style.display = "inline";
@@ -15,18 +15,3 @@ function toggleReadMore(moreId, cutId, buttonId) {
   btnText.style.display = "inline"; // Show the button after click
 }
 
-
-var slideIndex = 0;
-showSlides();
-
-function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {slideIndex = 1}
-    slides[slideIndex-1].style.display = "block";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
